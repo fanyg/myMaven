b08436bc3957cbbdb6a7df9ad1dae3ffd47631f3
fyg_bxbank <yonggang.fan@pactera.com>
2017-10-10 18:52:20 +0800

    update

---
+++ src/test/java/com/pactera/junit/UserServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.pactera.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.pactera.service.IUserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:/spring-mvc.xml","classpath*:/spring-mybatis.xml"})
+public class UserServiceImplTest extends AbstractJUnit4SpringContextTests {
+
+	@Autowired
+	public IUserService userService;
+	@Test
+	public void testUserServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+}
